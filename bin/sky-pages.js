e6f0524a45c4d6a651e7d99b56cf1f0dfba1d8fc
Bobby Earl <bobby.earl@blackbaud.com>
2016-09-06 10:37:50 -0400

    Refactored out webpack-specific logic

--- bin/sky-pages.js
+++ bin/sky-pages.js
@@ -2,6 +2,6 @@
 'use strict';
 
 const minimist = require('minimist');
-const cli = require('../lib/cli');
+const cli = require('../index');
 
 cli(minimist(process.argv.slice(2)));
---
+++ index.js
@@ -0,0 +1,62 @@
+/*jshint node: true*/
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const logger = require('winston');
+
+/**
+ * Iterates object's devDependencies to find applicable modules.
+ * @name getModules
+ * @returns [module[]] modules
+ */
+const getModules = (packageJson) => {
+  let modules = [];
+
+  if (packageJson.devDependencies) {
+    for (let d in packageJson.devDependencies) {
+      if (/(.*)-sky-pages-out-(.*)/gi.test(d)) {
+        modules.push(require(path.join(process.cwd(), 'node_modules', d)));
+      }
+    }
+  }
+
+  return modules;
+};
+
+/**
+ * Iterates an array of modules.
+ * Executes the requested method if it exists.
+ * @name runCommand
+ * @returns null
+ */
+const runCommand = (modules, command, argv) => {
+  modules.forEach((module) => {
+    if (typeof module.runCommand === 'function') {
+      module.runCommand(command, argv);
+    }
+  });
+};
+
+/**
+ * Processes an argv object.
+ * Reads package.json if it exists.
+ * @name processArgv
+ * @param [Object] argv
+ */
+const processArgv = (argv) => {
+  const packageJsonPath = path.join(process.cwd(), 'package.json');
+  const command = argv._[0];
+
+  if (command === 'version') {
+    const packageJson = require(path.resolve(__dirname, 'package.json'));
+    logger.info('sky-pages-cli: %s', packageJson.version);
+  }
+
+  if (fs.existsSync(packageJsonPath)) {
+    const modules = getModules(require(packageJsonPath));
+    runCommand(modules, command, argv);
+  }
+};
+
+module.exports = processArgv;
