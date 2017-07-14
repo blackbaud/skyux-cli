730be4db79d7f2b1ed65cdb829551308c3502dda
Blackbaud-TommyVernieri <tommy.vernieri@blackbaud.me>
2017-07-14 16:49:30 -0400

    Adding Cobertura and JUnit test results output

---
+++ test/jasmine-junit-reporter.spec.js
@@ -0,0 +1,9 @@
+﻿/*jshint jasmine: true, node: true */
+'use strict';
+
+const jasmineReporters = require('jasmine-reporters');
+
+jasmine.getEnv().addReporter(
+    new jasmineReporters.JUnitXmlReporter({
+        savePath: 'testresults'
+    }));
