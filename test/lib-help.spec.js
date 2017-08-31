a9777b472e6f727a4ff86020d620ae84c485065b
John Lyons <John.Lyons@blackbaud.com>
2017-08-31 15:48:04 -0400

    Docs reorder help (#128)
    * Added support for help topics
    
    * Update CLI help.
    
    * Fixed filename.  Slightly reworked messaging
    
    * Flesh out description.

--- test/lib-help.spec.js
+++ test/lib-help.spec.js
@@ -1,11 +1,38 @@
 /*jshint jasmine: true, node: true */
 'use strict';
 
+const fs = require('fs-extra');
+
 describe('skyux help command', () => {
   it('should log the help information', () => {
     spyOn(console, 'log');
     const help = require('../lib/help');
-    help();
+    help({ _: [] });
     expect(console.log).toHaveBeenCalled();
+  });
+
+  it('should handle known help topics', () => {
+
+    const topic = 'CUSTOM HELP TOPIC';
+    spyOn(console, 'log');
+    spyOn(fs, 'existsSync').and.returnValue(true);
+    spyOn(fs, 'readFileSync').and.returnValue(topic);
+
+    const help = require('../lib/help');
+    help({ _: ['help', 'build'] });
+
+    expect(console.log.calls.mostRecent().args[0]).toBe(topic);
+  });
+
+  it('should handle unknown help topics', () => {
+    const topic = 'DEFAULT HELP TOPIC';
+    spyOn(console, 'log');
+    spyOn(fs, 'existsSync').and.returnValue(false);
+    spyOn(fs, 'readFileSync').and.returnValue(topic);
+
+    const help = require('../lib/help');
+    help({ _: ['help', 'fake-call-does-not-exist'] });
+
+    expect(console.log.calls.mostRecent().args[0]).toBe(topic);
   });
 });
