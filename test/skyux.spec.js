00b1c31ef1a0f2347624d45f89fa8f2f5136544c
Bobby Earl <bobby.earl@blackbaud.com>
2017-10-31 09:31:21 -0400

    Adjusted glob pattern.  Cleaned up tests

--- test/skyux.spec.js
+++ test/skyux.spec.js
@@ -3,13 +3,70 @@
 
 const fs = require('fs');
 const path = require('path');
+const glob = require('glob');
 const mock = require('mock-require');
 const proxyquire = require('proxyquire');
 const logger = require('winston');
 
-describe('skyux CLI', () => {
+fdescribe('skyux CLI', () => {
+
+  afterEach(() => {
+    mock.stopAll();
+  });
+
+  function setupMock(noNameProperty) {
+    const cwd = 'current-working-directory';
+    spyOn(process, 'cwd').and.returnValue(cwd);
+
+    mock('path', {
+      dirname: (dir) => dir.replace('/package.json', ''),
+      join: (dir, pattern) => `${dir}/${pattern}`
+    });
+
+    if (noNameProperty) {
+      mock('local-module/package.json', {});
+      mock('non-scoped-global-module/package.json', {});
+      mock('scoped-global-module/package.json', {});
+    } else {
+      mock('local-module/package.json', {
+        name: 'local-module-name'
+      });
+      mock('non-scoped-global-module/package.json', {
+        name: 'non-scoped-global-module-name'
+      });
+      mock('scoped-global-module/package.json', {
+        name: 'scoped-global-module-name'
+      });
+    }
+
+    mock('glob', {
+      sync: (pattern) => {
+
+        // Emulates local package installed
+        if (pattern.indexOf(cwd) > -1) {
+          return [
+            'local-module/package.json'
+          ];
+
+        // Emulates global package that's not scoped to @blackbaud
+        } else if (pattern.indexOf('../..') > -1) {
+          return [
+            'non-scoped-global-module/package.json'
+          ];
+
+        // Emulates global package that's scoped
+        } else {
+          return [
+            'scoped-global-module/package.json'
+          ];
+        }
+      }
+    });
+  }
 
   it('should accept known command version', () => {
+    spyOn(glob, 'sync').and.returnValue([]);
+
     let called = false;
     mock('../lib/version', {
       logVersion: () => {
@@ -23,6 +80,8 @@ describe('skyux CLI', () => {
   });
 
   it('should accept the -v flag', () => {
+    spyOn(glob, 'sync').and.returnValue([]);
+
     let called = false;
     mock('../lib/version', {
       logVersion: () => {
@@ -36,6 +95,8 @@ describe('skyux CLI', () => {
   });
 
   it('should accept known command help', () => {
+    spyOn(glob, 'sync').and.returnValue([]);
+
     let called = false;
     mock('../lib/help', () => {
       called = true;
@@ -47,6 +108,8 @@ describe('skyux CLI', () => {
   });
 
   it('should accept the -h flag', () => {
+    spyOn(glob, 'sync').and.returnValue([]);
+
     let called = false;
     mock('../lib/help', () => {
       called = true;
@@ -58,6 +121,8 @@ describe('skyux CLI', () => {
   });
 
   it('should default to the help command', () => {
+    spyOn(glob, 'sync').and.returnValue([]);
+
     let called = false;
     mock('../lib/help', () => {
       called = true;
@@ -69,6 +134,8 @@ describe('skyux CLI', () => {
   });
 
   it('should accept known command new', () => {
+    spyOn(glob, 'sync').and.returnValue([]);
+
     let called = false;
     mock('../lib/new', () => {
       called = true;
@@ -80,61 +147,32 @@ describe('skyux CLI', () => {
   });
 
   it('should accept unknown command', () => {
+    spyOn(glob, 'sync').and.returnValue([]);
     spyOn(logger, 'info');
 
     const cli = mock.reRequire('../index');
-    cli({ _: ['test'] });
+    cli({ _: ['unknownCommand'] });
     expect(logger.info).toHaveBeenCalled();
   });
-
-  function setupMock(noNameProperty) {
-    const cwd = 'current-working-directory';
-    spyOn(process, 'cwd').and.returnValue(cwd);
-
-    mock('path', {
-      dirname: (dir) => dir.replace('/package.json', ''),
-      join: (dir, pattern) => `${dir}/${pattern}`
-    });
-
-    if (noNameProperty) {
-      mock('module-in-cwd/package.json', {});
-      mock('module-not-in-cwd/package.json', {});
-    } else {
-      mock('module-in-cwd/package.json', {
-        name: 'module-in-cwd-name'
-      });
-      mock('module-not-in-cwd/package.json', {
-        name: 'module-not-in-cwd-name'
-      });
-    }
-
-    mock('glob', {
-      sync: (pattern) => {
-        if (pattern.indexOf(cwd) > -1) {
-          return [
-            'module-in-cwd/package.json'
-          ];
-        } else {
-          return [
-            'module-not-in-cwd/package.json'
-          ];
-        }
-      }
-    });
-  }
 
   it('should look globally and locally for matching glob patterns', () => {
     spyOn(logger, 'info');
     setupMock();
     const customCommand = 'customCommand';
 
-    mock('module-in-cwd', {
+    mock('local-module', {
       runCommand: (cmd) => {
         expect(cmd).toBe(customCommand);
       }
     });
 
-    mock('module-not-in-cwd', {
+    mock('non-scoped-global-module', {
+      runCommand: (cmd) => {
+        expect(cmd).toBe(customCommand);
+      }
+    });
+
+    mock('scoped-global-module', {
       runCommand: (cmd) => {
         expect(cmd).toBe(customCommand);
       }
@@ -142,42 +180,58 @@ describe('skyux CLI', () => {
 
     const cli = mock.reRequire('../index');
     cli({ _: [customCommand] });
-    expect(logger.info).toHaveBeenCalledWith(`Passing command to module-in-cwd-name`);
-    expect(logger.info).toHaveBeenCalledWith(`Passing command to module-not-in-cwd-name`);
+    expect(logger.info).toHaveBeenCalledWith(`Passing command to local-module-name`);
+    expect(logger.info).toHaveBeenCalledWith(`Passing command to non-scoped-global-module-name`);
+    expect(logger.info).toHaveBeenCalledWith(`Passing command to scoped-global-module-name`);
   });
 
   it('should handle an error when requiring a malformed module', () => {
-
-    // not mocking module-not-in-cwd to simulate error
-    mock.stopAll();
-
     setupMock();
     const customCommand = 'customCommand';
 
-    mock('module-in-cwd', {
+    // not mocking global modules to simulate error
+    mock('local-module', {
       runCommand: (cmd) => {
         expect(cmd).toBe(customCommand);
       }
     });
 
-    spyOn(logger, 'error');
+    spyOn(logger, 'info');
 
     const cli = mock.reRequire('../index');
     cli({ _: [customCommand] });
 
-    expect(logger.error).toHaveBeenCalledWith(
-      `Error loading module: module-not-in-cwd/package.json`
+    expect(logger.info).toHaveBeenCalledWith(
+      `Error loading module: non-scoped-global-module/package.json`
     );
 
   });
 
-  it('should not log package name if property does not exist', () => {
+  it('should use path if name property does not exist in package.json', () => {
     spyOn(logger, 'info');
     setupMock(true);
+
+    mock('local-module', {
+      runCommand: () => {}
+    });
+
+    mock('non-scoped-global-module', {
+      runCommand: () => {}
+    });
+
+    mock('scoped-global-module', {
+      runCommand: () => {}
+    });
+
     const cli = mock.reRequire('../index');
     cli({ _: ['customCommand'] });
-    expect(logger.info).not.toHaveBeenCalledWith(`Passing command to module-in-cwd-name`);
-    expect(logger.info).not.toHaveBeenCalledWith(`Passing command to module-not-in-cwd-name`);
+
+    expect(logger.info).not.toHaveBeenCalledWith(`Passing command to local-module-name`);
+    expect(logger.info).not.toHaveBeenCalledWith(`Passing command to non-scoped-global-module-name`);
+    expect(logger.info).not.toHaveBeenCalledWith(`Passing command to scoped-global-module-name`);
+    expect(logger.info).toHaveBeenCalledWith(`Passing command to local-module`);
+    expect(logger.info).toHaveBeenCalledWith(`Passing command to non-scoped-global-module`);
+    expect(logger.info).toHaveBeenCalledWith(`Passing command to scoped-global-module`);
   });
 
 });
